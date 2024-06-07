c23f63c8a440bd6282415c431bfb73844c13cc80
esther2145 <kicaest03@gmail.com>
2024-06-07 23:00:21 +0300

    Add files via upload

---
+++ operation.py
@@ -0,0 +1,42 @@
+"""
+# assigning variables 
+a = 4+3
+print(a)
+# shows the remainder of the values divided
+divide = 25%2
+print(divide)
+#makes a power of a number
+b = 5**2
+print(b)
+# shows the answer without the remainder
+c = 25//2
+print(c)
+#short form of assignment
+#getting the short form of a = a*2
+a *= 2
+print(a)
+#getting the short form of a = a-3
+a -= 3
+print(a)
+# getting the short form of a = a**3
+a **= 3
+print(a)
+# showing one variable is bigger,equal,not equal to
+j =18
+m =22
+name = "tobby"
+print(m>j)
+print(m==j)
+print(j!=m)
+#adding on conditions
+print((m>=18) and (m>j))
+print((m>=18)or(m>j)and(m!=21))
+print((m>=18)and(m>j)and(name!="tobby"))
+# "is " is used to tell if identical
+print(m is not j)
+print(m is j)
+"""
+# checking if g is in message (checking if something is inside something)
+message = "nigga"
+print('g' in message)
+print('c' in message)
---
+++ work.R
@@ -0,0 +1,19 @@
+division= 90/34
+library(readxl)
+install.packages("ggplot2")
+library(ggplot2)
+datasets::Titanic
+#check
+?Titanic
+datasets::airquality
+?airquality
+summary(airquality)
+data = airquality
+mean(airquality$Wind)
+
+remove.packages("ggplot2")
+install.packages("ggplot2")
+library(ggplot2)
+summary(Female_Mps)
+mean(data)
+col
